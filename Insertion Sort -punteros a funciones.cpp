14257a13876408f8060b6ea54b3a8b50117e6d77
Linghai Joaquin, Zhong Callasi <104087488+Sommerfield3@users.noreply.github.com>
2022-07-02 23:57:15 -0500

    Add files via upload

---
+++ Insertion Sort -punteros a funciones.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+class prueba{
+public:
+	string nombre;
+	int numero;
+	bool operator>(prueba &b);
+	bool operator<=(prueba &b);
+};
+bool prueba::operator>(prueba &b){
+	return numero>b.numero;
+}
+bool prueba::operator<=(prueba &b){
+	return numero<=b.numero;
+}
+template <typename T>
+void imprimir(T lista[], int tam){
+	for (int i=0;i<tam;i++){
+		if (tam==1){
+			cout<<lista[i];
+		}
+		else if (i==0){
+			cout<<"["<<lista[i];
+		}
+		else if (i==(tam-1)){
+			cout<<", "<<lista[i]<<"]";
+		}
+		else{
+			cout<<", "<<lista[i];
+		}
+	}
+	cout<<endl;
+}
+void imprimirObjetosPrueba(prueba lista[], int tam){
+	for (int i=0;i<tam;i++){
+		if (tam==1){
+			cout<<lista[i].numero;
+		}
+		else if (i==0){
+			cout<<"["<<lista[i].numero;
+		}
+		else if (i==(tam-1)){
+			cout<<", "<<lista[i].numero<<"]";
+		}
+		else{
+			cout<<", "<<lista[i].numero;
+		}
+	}
+	cout<<endl;
+}
+
+template <typename T>
+bool descendente(T a, T b){
+	return a>b;
+}
+template <typename T>
+bool ascendente(T a, T b){
+	return a<=b;
+}
+template <typename T>	
+void insertionSort(T lista[],int tam, bool (*funcion)(T,T)){/*Esta función es usada para ordenar de menor a mayor*/
+	int i=1;
+	int j;
+	while (i<tam){
+		T menMay;
+		menMay=lista[i];
+		j=i-1;
+		while (j>=0 && funcion(menMay,lista[j])){
+			lista[j+1]=lista[j];
+			j--;
+		}
+		lista[j+1]=menMay;/*
+		cout<<"Búsqueda desde el elemento actual ("<<i<<"): ";
+		void (*impr)(int[],int);
+		impr=&imprimir;
+		(*impr)(lista, tam);
+		cout<<endl;*/
+		i++;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	prueba *lista=new prueba[6];
+	lista[0].nombre="a";
+	lista[1].nombre="b";
+	lista[2].nombre="c";
+	lista[3].nombre="d";
+	lista[4].nombre="e";
+	lista[5].nombre="f";
+	lista[0].numero=661;
+	lista[1].numero=348;
+	lista[2].numero=845;
+	lista[3].numero=969;
+	lista[4].numero=765;
+	lista[5].numero=137;
+	int lista1[]={23,432,325,45,5,25};
+	float lista2[]={88.23,52.12,75.56,4.98,97.655,97.5,100.34};
+	int tam1=sizeof(lista1) / sizeof(lista1[0]);/*Tamaño del array*/
+	int tam2=sizeof(lista2) / sizeof(lista2[0]);
+	cout<<"La lista de objetos inicial (se muestra el numero) es: ";
+	imprimirObjetosPrueba(lista, 6);/*Imprimimos la lista final*/
+	cout<<"La lista 1 inicial es: ";
+	imprimir(lista1, tam1);
+	cout<<"La lista 2 inicial es: ";
+	imprimir(lista2, tam2);
+	insertionSort<int>(lista1,tam1,descendente);
+	insertionSort<float>(lista2,tam2,ascendente);
+	insertionSort<prueba>(lista,6,descendente);
+	cout<<"La lista de objetos final (Descendente) es: ";
+	imprimirObjetosPrueba(lista, 6);/*Imprimimos la lista final*/
+	cout<<"La lista 1 final (Descendente) es: ";
+	imprimir(lista1, tam1);
+	cout<<"La lista 2 final (Ascendente) es: ";
+	imprimir(lista2, tam2);
+	delete [] lista;
+	system("pause");
+	return 0;
+}
---
+++ Merge sort - punteros a funciones.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+using namespace std;
+class prueba2{
+public:
+	string nombre;
+	int numero;
+	bool operator>(prueba2 &b);
+	bool operator<=(prueba2 &b);
+};
+bool prueba2::operator>(prueba2 &b){
+	return numero>b.numero;
+}
+bool prueba2::operator<=(prueba2 &b){
+	return numero<=b.numero;
+}
+template <typename T>
+bool descendente(T a, T b){
+	return a>b;
+}
+template <typename T>
+	bool ascendente(T a, T b){
+	return a<=b;
+}
+template <typename T>
+void merge(T lista[],int izq,int med, int der, bool (*func)(T,T)){
+	auto const lista1Tam=med-izq+1;
+	auto const lista2Tam=der-med;
+	T *listaIzq=new T[lista1Tam];
+	T *listaDer=new T[lista2Tam];
+	for (int i=0;i<lista1Tam;i++){
+		listaIzq[i]=lista[izq+i]; /*Recorremos todas las posiciones de listaIzq llenando con elementos de lista desde la parte inicial indicada.*/
+	}
+	for (int j=0;j<lista2Tam;j++){
+		listaDer[j]=lista[med+1+j]; 
+	}
+	int lista1PosActual=0;
+	int lista2PosActual=0;
+	int MergedPosActual=izq;
+	while (lista1PosActual<lista1Tam && lista2PosActual<lista2Tam){
+		if ((*func)(listaIzq[lista1PosActual],listaDer[lista2PosActual])){
+			lista[MergedPosActual]=listaIzq[lista1PosActual];
+			lista1PosActual++;
+		}
+		else{
+			lista[MergedPosActual]=listaDer[lista2PosActual];
+			lista2PosActual++;
+		}
+		MergedPosActual++;
+	}
+	while (lista1PosActual<lista1Tam){
+		lista[MergedPosActual]=listaIzq[lista1PosActual];
+		lista1PosActual++;
+		MergedPosActual++;
+	}
+	while (lista2PosActual<lista2Tam){
+		lista[MergedPosActual]=listaDer[lista2PosActual];
+		lista2PosActual++;
+		MergedPosActual++;
+	}
+	delete [] listaIzq;
+	delete []listaDer;
+}	
+template <typename T>
+void mergeSort(T lista[],int inic,int fin, bool (*func)(T,T)){
+	if (inic<fin){
+		int med=inic+(fin-inic)/2;
+		void (*mismo)(T[],int,int, bool (*func)(T,T));
+		void (*base)(T[],int,int,int,bool (*func)(T,T));
+		mismo=&mergeSort;
+		base=&merge;
+		(*mismo)(lista,inic,med,func);
+		(*mismo)(lista,med+1,fin,func);
+		(*base)(lista,inic,med,fin,func);
+	}
+}
+void imprimir(int lista[], int tam){
+	for (int i=0;i<tam;i++){
+		if (tam==1){
+			cout<<lista[i];
+		}
+		else if (i==0){
+			cout<<"["<<lista[i];
+		}
+		else if (i==(tam-1)){
+			cout<<", "<<lista[i]<<"]";
+		}
+		else{
+			cout<<", "<<lista[i];
+		}
+	}
+}
+int main(int argc, char *argv[]) {
+	int lista[]={45,3342,23,34,23,4};
+	int tam=sizeof(lista)/sizeof(lista[0]);
+	cout<<"Lista inicial: "<<endl;
+	imprimir(lista, tam);
+	cout<<endl;
+	cout<<"Procesando..."<<endl;
+	mergeSort(lista,0,tam-1,ascendente); /*Aplicamos el ordenamiento.*/
+	cout<<"Lista procesada"<<endl;
+	cout<<"Lista resultante: "<<endl;
+	imprimir(lista, tam);
+	cout<<endl;
+	system("pause");
+	return 0;
+}
+
